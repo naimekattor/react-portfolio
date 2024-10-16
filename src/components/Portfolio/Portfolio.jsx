2a8e2ac3e6d297fad0225e5b65b8319a62c9af47
Naimur Rahman Naim <56504158+naimekattor@users.noreply.github.com>
2024-10-16 18:53:13 +0600

    Add files via upload

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,40 +1,88 @@
-import React, { useContext } from "react";
+import { useContext, useState } from "react";
 import "swiper/css";
 import "./Portfolio.css";
-import { Swiper, SwiperSlide } from "swiper/react";
-import Sidebar from "./../../img/sidebar.png";
-import Ecommerce from "./../../img/ecommerce.png";
-import HOC from "./../../img/hoc.png";
-import MusicApp from "./../../img/musicapp.png";
 import { themeContext } from "../../Context";
-
+import { gallerydata } from "./../../galleryData";
 const Portfolio = () => {
+  const [data, setData] = useState(gallerydata);
+  const [catagory, setCategory] = useState("All");
   const theme = useContext(themeContext);
   const darkMode = theme.state.darkMode;
+  const filterItem = (categoryName) => {
+    const updatedItems = gallerydata.filter(
+      (curElem) => curElem.category === categoryName
+    );
+    setData(updatedItems);
+  };
+  /* useEffect(() => {
+    () => {
+      category === "All"
+        ? setData(gallerydata)
+        : setData(gallerydata.filter((curelem) => curelem.category));
+    };
+  }, [category]); */
   return (
     <div className="portfolio" id="Portfolio">
       <span style={{ color: darkMode ? "white" : "" }}>Recent Project</span>
       <span>Portfolio</span>
-      {/* slider */}
-      <Swiper
-        spaceBetween={30}
-        slidesPerView={3}
-        grabCursor={true}
-        className="portfolio-slider"
-      >
-        <SwiperSlide>
-          <img src={Sidebar} alt="" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={Ecommerce} alt="" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={HOC} alt="" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={MusicApp} alt="" />
-        </SwiperSlide>
-      </Swiper>
+
+      <div className="p-tabs">
+        <ul>
+          <li
+            className={catagory == "All" ? "active" : ""}
+            onClick={() => {
+              setData(gallerydata);
+              setCategory("All");
+            }}
+          >
+            All
+          </li>
+          <li
+            className={catagory == "LOGO" ? "active" : ""}
+            onClick={() => {
+              filterItem("logo");
+              setCategory("LOGO");
+            }}
+          >
+            LOGO
+          </li>
+          <li
+            className={catagory == "VIDEO" ? "active" : ""}
+            onClick={() => {
+              filterItem("video");
+              setCategory("VIDEO");
+            }}
+          >
+            VIDEO
+          </li>
+          <li
+            className={catagory == "WEB DESIGN" ? "active" : ""}
+            onClick={() => {
+              filterItem("web design");
+              setCategory("WEB DESIGN");
+            }}
+          >
+            WEB DESIGN
+          </li>
+          <li
+            className={catagory == "WEB DEVELOPEMENT" ? "active" : ""}
+            onClick={() => {
+              filterItem("web developement");
+              setCategory("WEB DEVELOPEMENT");
+            }}
+          >
+            WEB DEVELOPEMENT
+          </li>
+        </ul>
+
+        <div className="p-images">
+          {data.map((curElem) => (
+            <div className="p-tab-content" key={curElem.id}>
+              <img src={curElem.url} alt="" />
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
