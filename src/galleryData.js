a6715d2cca247e42e0a1f5e10724046580eb4fa6
Naimur Rahman Naim <56504158+naimekattor@users.noreply.github.com>
2024-10-16 18:52:00 +0600

    Add files via upload

---
+++ src/galleryData.js
@@ -0,0 +1,39 @@
+export const gallerydata=[{
+  id:"1",
+  category:"logo",
+  url:"https://images.unsplash.com/photo-1490971588422-52f6262a237a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjN8fHZpZGVvfGVufDB8fDB8fHww"
+},
+{
+  id:"2",
+  category:"web design",
+  url:"https://images.unsplash.com/photo-1640955014216-75201056c829?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8cGN8ZW58MHx8MHx8fDA%3D"
+},
+{
+  id:"3",
+  category:"web developement",
+  url:"https://images.unsplash.com/photo-1603969409447-ba86143a03f6?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cGN8ZW58MHx8MHx8fDA%3D"
+},
+{
+  id:"4",
+  category:"logo",
+  url:"https://plus.unsplash.com/premium_photo-1661914978519-52a11fe159a7?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8bG9nb3xlbnwwfHwwfHx8MA%3D%3D"
+},{
+  id:"5",
+  category:"video",
+  url:"https://plus.unsplash.com/premium_photo-1678565546661-bf43274dd428?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fHBjfGVufDB8fDB8fHww"
+},
+{
+  id:"6",
+  category:"logo",
+  url:"https://images.unsplash.com/photo-1615915468538-0fbd857888ca?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8bG9nb3xlbnwwfHwwfHx8MA%3D%3D"
+},
+{
+  id:"7",
+  category:"logo",
+  url:"https://plus.unsplash.com/premium_photo-1682002135678-87b8a2fdde50?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8bG9nb3xlbnwwfHwwfHx8MA%3D%3D"
+},
+{
+  id:"8",
+  category:"web design",
+  url:"https://images.unsplash.com/photo-1585143042060-77fd3b53905a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjJ8fHBjfGVufDB8fDB8fHww"
+},]
