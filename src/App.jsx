5e796d8836b5f761179d22b13079256ea24420d2
Naimur Rahman Naim <56504158+naimekattor@users.noreply.github.com>
2024-10-20 02:18:20 +0600

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,22 @@ import Portfolio from "./components/Portfolio/Portfolio";
 import Testimonials from "./components/Testimonials/Testimonials";
 import Contact from "./components/Contact/Contact";
 import Footer from "./components/Footer/Footer";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { themeContext } from "./Context";
 
 function App() {
+  const [loading, setLoading] = useState(true);
   const theme = useContext(themeContext);
   const darkMode = theme.state.darkMode;
+  //loading App
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 5000);
+    return () => {
+      clearTimeout();
+    };
+  }, []);
   return (
     <div
       className="app"
@@ -23,15 +33,34 @@ function App() {
           : { color: "#000" }
       }
     >
-      <Navbar />
-      <Intro />
-      <MyServices />
-      <Experience />
-      <Works />
-      <Portfolio />
-      <Testimonials />
-      <Contact />
-      <Footer />
+      {loading ? (
+        <div>
+          <div className="loading">
+            <div className="loading__letter">L</div>
+            <div className="loading__letter">o</div>
+            <div className="loading__letter">a</div>
+            <div className="loading__letter">d</div>
+            <div className="loading__letter">i</div>
+            <div className="loading__letter">n</div>
+            <div className="loading__letter">g</div>
+            <div className="loading__letter">.</div>
+            <div className="loading__letter">.</div>
+            <div className="loading__letter">.</div>
+          </div>
+        </div>
+      ) : (
+        <>
+          <Navbar />
+          <Intro />
+          <MyServices />
+          <Experience />
+          <Works />
+          <Portfolio />
+          <Testimonials />
+          <Contact />
+          <Footer />
+        </>
+      )}
     </div>
   );
 }
