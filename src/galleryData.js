82b5fc6c46da9ae7700cbe5e04f757f9b9c6e3a2
Naimur Rahman Naim <56504158+naimekattor@users.noreply.github.com>
2024-10-20 02:18:40 +0600

    Add files via upload

--- src/galleryData.js
+++ src/galleryData.js
@@ -1,39 +1,63 @@
+import socialmedia from "./../src/img/socialmedia.png"
+import calculator from "./../src/img/calculator.png"
+import progressbar from "./../src/img/progressbar.png"
+import tictactoe from "./../src/img/tictactoe.png"
+import quizzapp from "./../src/img/quizzapp.png"
+import accordion from "./../src/img/accordion.png"
+
 export const gallerydata=[{
   id:"1",
   category:"logo",
-  url:"https://images.unsplash.com/photo-1490971588422-52f6262a237a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjN8fHZpZGVvfGVufDB8fDB8fHww"
+  url:socialmedia,
+  webUrl:"https://naimekattor.github.io/react-socialMedia/",
+  project:"Social Media"
 },
 {
   id:"2",
   category:"web design",
-  url:"https://images.unsplash.com/photo-1640955014216-75201056c829?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8cGN8ZW58MHx8MHx8fDA%3D"
+  url:calculator,
+  webUrl:"https://naimekattor.github.io/react-calculator/",
+  project:"Calculator App"
 },
 {
   id:"3",
   category:"web developement",
-  url:"https://images.unsplash.com/photo-1603969409447-ba86143a03f6?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cGN8ZW58MHx8MHx8fDA%3D"
+  url:quizzapp,
+  webUrl:"https://naimekattor.github.io/react-quizApp/",
+  project:"Quiz App"
 },
 {
   id:"4",
   category:"logo",
-  url:"https://plus.unsplash.com/premium_photo-1661914978519-52a11fe159a7?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8bG9nb3xlbnwwfHwwfHx8MA%3D%3D"
-},{
+  url:progressbar,
+  webUrl:"https://naimekattor.github.io/react-progressbar/",
+  project:"Progress Bar "
+},
+{
   id:"5",
   category:"video",
-  url:"https://plus.unsplash.com/premium_photo-1678565546661-bf43274dd428?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fHBjfGVufDB8fDB8fHww"
+  url:tictactoe,
+  webUrl:"https://naimekattor.github.io/react-tictactoe/",
+  project:"tic Tac Toe App"
 },
 {
   id:"6",
   category:"logo",
-  url:"https://images.unsplash.com/photo-1615915468538-0fbd857888ca?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8bG9nb3xlbnwwfHwwfHx8MA%3D%3D"
+  url:accordion,
+  webUrl:"https://naimekattor.github.io/react-accordionApp/",
+  project:"Accordion App"
 },
 {
   id:"7",
-  category:"logo",
-  url:"https://plus.unsplash.com/premium_photo-1682002135678-87b8a2fdde50?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8bG9nb3xlbnwwfHwwfHx8MA%3D%3D"
+  category:"web developement",
+  url:socialmedia,
+  webUrl:"https://naimekattor.github.io/react-socialMedia/",
+  project:"Social Media"
 },
 {
   id:"8",
   category:"web design",
-  url:"https://images.unsplash.com/photo-1585143042060-77fd3b53905a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjJ8fHBjfGVufDB8fDB8fHww"
+  url:quizzapp,
+  webUrl:"https://naimekattor.github.io/react-quizApp/",
+  project:"Quiz App"
 },]
