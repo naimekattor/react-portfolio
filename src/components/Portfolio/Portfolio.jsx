2b1fa440b72c321646292eb5e887273bd051b43f
Naimur Rahman Naim <56504158+naimekattor@users.noreply.github.com>
2024-10-20 02:20:52 +0600

    Add files via upload

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -3,9 +3,14 @@ import "swiper/css";
 import "./Portfolio.css";
 import { themeContext } from "../../Context";
 import { gallerydata } from "./../../galleryData";
+import Modal from "../../Modal/Modal";
 const Portfolio = () => {
   const [data, setData] = useState(gallerydata);
   const [catagory, setCategory] = useState("All");
+  const [showModal, setShowModal] = useState(false);
+  const [webUrl, setWebUrl] = useState("");
+  const [imgUrl, setimgUrl] = useState("");
+  const [project, setProject] = useState("");
   const theme = useContext(themeContext);
   const darkMode = theme.state.darkMode;
   const filterItem = (categoryName) => {
@@ -14,13 +19,16 @@ const Portfolio = () => {
     );
     setData(updatedItems);
   };
-  /* useEffect(() => {
-    () => {
-      category === "All"
-        ? setData(gallerydata)
-        : setData(gallerydata.filter((curelem) => curelem.category));
-    };
-  }, [category]); */
+  //Modal popup
+  const handleOpen = (id, weburl, imgUrl, project) => {
+    setShowModal(true);
+    setWebUrl(weburl);
+    setimgUrl(imgUrl);
+    setProject(project);
+  };
+  const handleClode = () => {
+    setShowModal(false);
+  };
   return (
     <div className="portfolio" id="Portfolio">
       <span style={{ color: darkMode ? "white" : "" }}>Recent Project</span>
@@ -78,9 +86,28 @@ const Portfolio = () => {
         <div className="p-images">
           {data.map((curElem) => (
             <div className="p-tab-content" key={curElem.id}>
-              <img src={curElem.url} alt="" />
+              <img
+                src={curElem.url}
+                alt=""
+                onClick={() => {
+                  handleOpen(
+                    curElem.id,
+                    curElem.webUrl,
+                    curElem.url,
+                    curElem.project
+                  );
+                }}
+              />
             </div>
           ))}
+          {showModal && (
+            <Modal
+              close={handleClode}
+              website={webUrl}
+              imgUrl={imgUrl}
+              project={project}
+            />
+          )}
         </div>
       </div>
     </div>
